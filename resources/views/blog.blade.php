7ac72c7bc0b00770b333732bbea1ec25ce8d91ff
5026241205Sabela <5026241205@student.its.ac.id>
2026-04-28 19:45:40 +0700

    pertemuan 8

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,17 +1,5 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Tutorial Laravel - www.malasngoding.com</title>
-</head>
-<body>
-
-	<h3>www.malasngoding.com</h3>
-	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
-	<p>Ini adalah view blog. ada di route blog.</p>
-
-</body>
-</html><!DOCTYPE html>
-<html>
 
 <head>
     <title>Tutorial Laravel - www.malasngoding.com</title>
